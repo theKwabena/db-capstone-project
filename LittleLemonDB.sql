90e0246afb7073e2f6b63604171a29fafa14ce41
theKwabena <abbeyemmanueltetteh@gmail.com>
2023-05-04 12:46:15 +0000

    Stored Procedures and Prepared Statements

--- LittleLemonDB.sql
+++ LittleLemonDB.sql
@@ -1,131 +1,20 @@
--- MySQL Workbench Forward Engineering
+-- create procedure getMaxQuantity()
+-- select  max(Quantity) from Orders
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema LittleLemonDB
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema LittleLemonDB
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `LittleLemonDB` DEFAULT CHARACTER SET utf8 ;
-USE `LittleLemonDB` ;
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`Customers`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Customers` (
-  `CustomerID` INT NOT NULL,
-  `FirstName` VARCHAR(45) NOT NULL,
-  `LastName` VARCHAR(45) NOT NULL,
-  `ContactNumber` VARCHAR(45) NOT NULL,
-  `Email` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`CustomerID`),
-  UNIQUE INDEX `ContactNumber_UNIQUE` (`ContactNumber` ASC) VISIBLE,
-  UNIQUE INDEX `Email_UNIQUE` (`Email` ASC) VISIBLE)
-ENGINE = InnoDB;
+-- call getMaxQuantity()
 
 
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`Staff`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Staff` (
-  `StaffID` INT NOT NULL,
-  `Name` VARCHAR(45) NOT NULL,
-  `Role` VARCHAR(45) NOT NULL,
-  `AnnualSalary` VARCHAR(45) NOT NULL,
-  UNIQUE INDEX `StaffID_UNIQUE` (`StaffID` ASC) VISIBLE,
-  PRIMARY KEY (`StaffID`))
-ENGINE = InnoDB;
+-- prepare GetOrderDetails from "Select OrderID, Quantity, Cost from Orders where BookingID = ? ";
 
+-- set @id =1;
+-- execute GetOrderDetails using @id
 
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`Bookings`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Bookings` (
-  `BookingID` INT NOT NULL,
-  `TableNo` INT NOT NULL,
-  `CustomerID` INT NOT NULL,
-  `BookingDate` DATETIME NOT NULL,
-  `EmployeeID` INT NULL,
-  PRIMARY KEY (`BookingID`),
-  UNIQUE INDEX `BookingID_UNIQUE` (`BookingID` ASC) VISIBLE,
-  UNIQUE INDEX `CustomerID_UNIQUE` (`CustomerID` ASC) VISIBLE,
-  INDEX `employee_idx` (`EmployeeID` ASC) VISIBLE,
-  CONSTRAINT `customer`
-    FOREIGN KEY (`CustomerID`)
-    REFERENCES `LittleLemonDB`.`Customers` (`CustomerID`)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION,
-  CONSTRAINT `employee`
-    FOREIGN KEY (`EmployeeID`)
-    REFERENCES `LittleLemonDB`.`Staff` (`StaffID`)
-    ON DELETE SET NULL
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+DELIMITER $$
+create procedure CancelOrder(in id INT, out stment varchar(255))
+BEGIN
+select * from Orders where OrderID = id;
+set stment = concat("Order with id", id ,"deleted");
+select stment;
+END $$
 
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`MenuItems`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`MenuItems` (
-  `ItemID` INT NOT NULL,
-  `Name` VARCHAR(45) NOT NULL,
-  `Type` VARCHAR(45) NOT NULL,
-  `Price` DECIMAL(5,2) NOT NULL,
-  PRIMARY KEY (`ItemID`),
-  UNIQUE INDEX `ItemID_UNIQUE` (`ItemID` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`Menu`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Menu` (
-  `MenuID` INT NOT NULL,
-  `ItemID` INT NOT NULL,
-  `Cuisine` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`MenuID`),
-  UNIQUE INDEX `MenuID_UNIQUE` (`MenuID` ASC) VISIBLE,
-  INDEX `item_idx` (`ItemID` ASC) VISIBLE,
-  CONSTRAINT `item`
-    FOREIGN KEY (`ItemID`)
-    REFERENCES `LittleLemonDB`.`MenuItems` (`ItemID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`Orders`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Orders` (
-  `OrderID` INT NOT NULL,
-  `MenuID` INT NOT NULL,
-  `BookingID` INT NOT NULL,
-  `BillAmount` DECIMAL(5,2) NOT NULL,
-  `Quantity` INT NOT NULL,
-  `OrderDate` DATE NOT NULL,
-  PRIMARY KEY (`OrderID`),
-  UNIQUE INDEX `OrderID_UNIQUE` (`OrderID` ASC) VISIBLE,
-  INDEX `menu_idx` (`MenuID` ASC) VISIBLE,
-  INDEX `booking_idx` (`BookingID` ASC) VISIBLE,
-  CONSTRAINT `menu`
-    FOREIGN KEY (`MenuID`)
-    REFERENCES `LittleLemonDB`.`Menu` (`MenuID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `booking`
-    FOREIGN KEY (`BookingID`)
-    REFERENCES `LittleLemonDB`.`Bookings` (`BookingID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DELIMITER ;
